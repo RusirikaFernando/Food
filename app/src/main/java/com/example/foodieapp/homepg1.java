96c96ad4bbf1f665d8b31646d6cce9636e3de857
janitha-visna <116863443+janitha-visna@users.noreply.github.com>
2023-06-19 22:14:10 +0530

    okay

--- app/src/main/java/com/example/foodieapp/homepg1.java
+++ app/src/main/java/com/example/foodieapp/homepg1.java
@@ -2,7 +2,9 @@ package com.example.foodieapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class homepg1 extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class homepg1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepg1);
+    }
+    public void add1(View n){
+        Intent i = new Intent(this, homepg6.class);
+        startActivity(i);
+    }
+    public void backbutton1(View n){
+        Intent i = new Intent(this, homepage.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/example/foodieapp/loginPg1.java
+++ app/src/main/java/com/example/foodieapp/loginPg1.java
@@ -2,7 +2,9 @@ package com.example.foodieapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class loginPg1 extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class loginPg1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_pg1);
+
+
     }
+    public void signinbtn(View n){
+        Intent i = new Intent(this, homepage.class);
+        startActivity(i);
+    }
+
 }
